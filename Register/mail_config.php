abf0d2147db3273f060c8f3c92ba65d608b7ff62
Mahi-kumari <mahikumari1809@gmail.com>
2026-03-27 11:42:37 +0530

    changes

--- Register/mail_config.php
+++ Register/mail_config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    "api_key" => "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",
+    "api_key" => getenv('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'),
     "from_name" => "ICSS",
     "from_email" => "mahiku1826@gmail.com"
 ];
